c5955c3d14c042f429c922580763734e845f6711
Carlos A. Rodriguez <car3564@utexas.edu>
2014-06-17 20:08:26 -0500

    Ran Doxygen to create the html/ directory

---
+++ html/search/all_72.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['rcl',['rcl',['../Collatz_8h.html#a2a3eac1d65a7db150c1650eebe83b245',1,'Collatz.h']]],
+  ['runcollatz_2ec_2b_2b',['RunCollatz.c++',['../RunCollatz_8c_09_09.html',1,'']]]
+];
---
+++ html/search/functions_72.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['rcl',['rcl',['../Collatz_8h.html#a2a3eac1d65a7db150c1650eebe83b245',1,'Collatz.h']]]
+];
